3da9d376f1bb14dfd8b44e9913ab184cb577fd6b
Annalise Lariviere Brown <alarivierebr@gmail.com>
2026-08-26 03:41:10 +0100

    adjusting filepaths, tidying up

--- pipeline/project_config.sh
+++ pipeline/project_config.sh
@@ -12,33 +12,37 @@
 
 # ------------ Project directories -------------- #
 
-PROJECT_DIR="/mnt/autofs/data/userdata/project0076"
+PROJECT_DIR="/mnt/autofs/data/userdata/project0076/annalise"
 
-RESULTS_DIR="${PROJECT_DIR}/annalise/pipeline/results"
+RESULTS_DIR="${PROJECT_DIR}/filtering/pipeline/results"
 
 LOG_DIR="${RESULTS_DIR}/logs"
 
-SCRIPTS_DIR="${PROJECT_DIR}/annalise/pipeline/scripts"
+SCRIPTS_DIR="${PROJECT_DIR}/filtering/pipeline/scripts"
 
-GLNEXUS_DIR="${PROJECT_DIR}/annalise/GLNexus/output2"
+GLNEXUS_DIR="${PROJECT_DIR}/GLNexus/output2"
 
 MUTYPER_DIR="${RESULTS_DIR}/mutyper"
 
-FILTER_VARIANTS_SCRIPT="${PROJECT_DIR}/annalise/filtering/filter_variants.py"
+FILTER_VARIANTS_SCRIPT="${PROJECT_DIR}/filtering/filter_variants.py"
+
+SAMPLE_LIST_DIR="${SCRIPTS_DIR}/sample_lists"
 
 # ------------ ANCESTRAL GENOMES --------------#
 
-FELIDAE_ANC="${PROJECT_DIR}/Cats/ancestral_genomes/ANCESTRAL_GENOME_FELIDAE/ancestral/ancestral_genome/ancestral.fasta"
-FELIS_ANC="${PROJECT_DIR}/Cats/ancestral_genomes/ANCESTRAL_GENOME_FELIS/ancestral/ancestral_genome/ancestral.fasta"
+FELIDAE_ANC="/mnt/autofs/data/userdata/project0076/Cats/ancestral_genomes/ANCESTRAL_GENOME_FELIDAE/ancestral/ancestral_genome/ancestral.fasta"
+FELIS_ANC="/mnt/autofs/data/userdata/project0076/Cats/ancestral_genomes/ANCESTRAL_GENOME_FELIS/ancestral/ancestral_genome/ancestral.fasta"
 
 
 #------- Sample Lists -------- #
 
-QC_SAMPLES_TO_KEEP="${SCRIPTS_DIR}/sample_lists/samples_to_keep.txt"
+QC_SAMPLES_TO_KEEP="${SAMPLE_LIST_DIR}/samples_to_keep.txt"
 
-DOM_ONLY="${SCRIPTS_DIR}/sample_lists/domestic_only_samples.txt"
+DOM_ONLY="${SAMPLE_LIST_DIR}/domestic_only_samples.txt"
 
-WILD_ONLY="${SCRIPTS_DIR}/sample_lists/wild_only_samples.txt"
+WILD_ONLY="${SAMPLE_LIST_DIR}/wild_only_samples.txt"
+
+FILTERED_OUT="${SAMPLE_LIST_DIR}/filtered_samples.txt"
 
 #------ suffixes -----#
 
@@ -48,13 +52,16 @@ CHROM_SUFFIX="filtered.vcf.gz"
 
 PLINK_2_DIR="${RESULTS_DIR}/plink_filtered/filtered_pca"
 
-DOM_ONLY_DIR="${RESULTS_DIR}plink_filtered/dom_only"
+PLINK_3_DIR="${RESULTS_DIR}/plink_nO/nO_filtered_pca"
 
-WILD_ONLY_DIR="${RESULTS_DIR}/plink_filtered/wild_only"
+DOM_ONLY_DIR="${RESULTS_DIR}/plink_filtered/dom_only/nO_dom_only"
+
+WILD_ONLY_DIR="${RESULTS_DIR}/plink_filtered/wild_only/nO_wild_only"
+
 
 # ------------ Input/output files---------------#
 
-#Input for 3_outlier_filter.sh
+#Input for 3_multi_split.sh
 GLNEXUS_VCF="${GLNEXUS_DIR}/cohort.full.vcf.gz"
 #Input for 4_bcftools_norm and 4_plink_pca
 OUTLIERS_REMOVED_VCF="${RESULTS_DIR}/outliers.removed.vcf.gz"
@@ -62,9 +69,9 @@ OUTLIERS_REMOVED_VCF="${RESULTS_DIR}/outliers.removed.vcf.gz"
 MULTI_SPLIT_VCF="${RESULTS_DIR}/multi_split.vcf.gz"
 MULTI_SPLIT_INDEX="${RESULTS_DIR}/multi_split.vcf.gz.tbi"
 #Output for 5 and input for 6
-SPLIT_CHROM="${RESULTS_DIR}/filtered.vcf.gz"
+FILTER_VAR="${RESULTS_DIR}/filtered.vcf.gz"
 #Output for 6 and Input for 7
-BCFTOOLS_FILTER="${RESULTS_DIR}/final.full.vcf.gz"
+BCFTOOLS_FILTER="${RESULTS_DIR}/nO_final.full.vcf.gz"
 #input for 8 and output for 8
 FILTERED_PLINK="${PLINK_2_DIR}"
 #output for 9 and input for 10
--- pipeline/run_pipeline.sh
+++ pipeline/run_pipeline.sh
@@ -14,40 +14,38 @@
 #SBATCH --mail-user=3175404l@student.gla.ac.uk
 #SBATCH --mail-type=BEGIN,END,FAIL
 
-source /mnt/autofs/data/userdata/project0076/annalise/pipeline/project_config.sh
+
+source /mnt/autofs/data/userdata/project0076/annalise/filtering/pipeline/project_config.sh
 
 set -euo pipefail
 
 
-#job3=$(sbatch "${SCRIPTS_DIR}/3_outlier_remove.sh" | awk '{print $4}')
-#echo "Submitted 3_no_outlier_remove.sh t: ${job3}"
+#Runs every step in the pipeline aside from 1_glnexus.sh and 2_glnexus_concat.sh. Run 1 and 2 manually and then run this script at Step 3!
 
-#ob4=$(sbatch --dependency=afterok:${job3} "${SCRIPTS_DIR}/4_multi_split.sh" | awk '{print $4}')
-#echo "Submitted 4_multi_split.sh: ${job4}"
+job1=$(sbatch "${SCRIPTS_DIR}/3_multi_split.sh" | awk '{print $4}')
+echo "Submitted 3_multi_split.sh: ${job1}"
 
-job5=$(sbatch "${SCRIPTS_DIR}/5_filter_variants.sh" | awk '{print $4}')
-echo "Submitted 5_filter_variants.sh: ${job5}"
+job2=$(sbatch --dependency=afterok:${job1} "${SCRIPTS_DIR}/4_filter_variants.sh" | awk '{print $4}')
+echo "Submitted 4_filter_variants.sh: ${job2}"
 
-job6=$(sbatch --dependency=afterok:${job5} "${SCRIPTS_DIR}/6_bcftools_filter.sh" | awk '{print $4}')
-echo "Submitted 6_bcftools_filter.sh: ${job6}"
+job3=$(sbatch --dependency=afterok:${job2} "${SCRIPTS_DIR}/5_bcftools_filter.sh" | awk '{print $4}')
+echo "Submitted 5_bcftools_filter.sh: ${job3}"
 
-job7=$(sbatch --dependency=afterok:${job6} "${SCRIPTS_DIR}/7_plink_filtered.sh" | awk '{print $4}')
-echo "Submitted 7_plink_filtered.sh: ${job7}"
+job4=$(sbatch --dependency=afterok:${job3} "${SCRIPTS_DIR}/6_plink_filtered.sh" | awk '{print $4}')
+echo "Submitted 6_plink_filtered.sh: ${job4}"
 
-job8=$(sbatch --dependency=afterok:${job7} "${SCRIPTS_DIR}/8_dom_only_plink.sh" | awk '{print $4}')
-echo "Submitted 8_dom_only_plink.sh: ${job8}"
+job5=$(sbatch --dependency=afterok:${job4} "${SCRIPTS_DIR}/7_dom_only_plink.sh" | awk '{print $4}')
+echo "Submitted 7_dom_only_plink.sh: ${job5}"
 
-job9=$(sbatch --dependency=afterok:${job7} "${SCRIPTS_DIR}/8_wild_only_plink.sh" | awk '{print $4}')
-echo "Submitted 8_wild_only_plink.sh: ${job9}"
+job6=$(sbatch --dependency=afterok:${job4} "${SCRIPTS_DIR}/7_wild_only_plink.sh" | awk '{print $4}')
+echo "Submitted 7_wild_only_plink.sh: ${job6}"
 
-job10=$(sbatch --dependency=afterok:${job6} "${SCRIPTS_DIR}/9_annotate.sh" | awk '{print $4}')
-echo "Submitted 9_annotate.sh: ${job10}"
+job7=$(sbatch --dependency=afterok:${job4} "${SCRIPTS_DIR}/8_annotate.sh" | awk '{print $4}')
+echo "Submitted 8_annotate.sh: ${job7}"
 
-job11=$(sbatch --dependency=afterok:${job10} "${SCRIPTS_DIR}/10_mutyper_var.sh" | awk '{print $4}')
-echo "Submitted 10_mutyper_var.sh: ${job11}"
+job8=$(sbatch --dependency=afterok:${job7} "${SCRIPTS_DIR}/9_mutyper_var.sh" | awk '{print $4}')
+echo "Submitted 9_mutyper_var.sh: ${job8}"
 
-job12=$(sbatch --dependency=afterok:${job11} "${SCRIPTS_DIR}/11_mutyper_spec.sh" | awk '{print $4}')
-echo "Submitted 11_mutyper_spec.sh: ${job12}"
+job9=$(sbatch --dependency=afterok:${job8} "${SCRIPTS_DIR}/10_mutyper_spec.sh" | awk '{print $4}')
+echo "Submitted 10_mutyper_spec.sh: ${job9}"
 
-#job13=$(sbatch --dependency=afterok:${job3} "${SCRIPTS_DIR}/4_plink_1.sh" | awk '{print $4}')
-#echo "Submitted 4_plink_1.sh: ${job13}"
